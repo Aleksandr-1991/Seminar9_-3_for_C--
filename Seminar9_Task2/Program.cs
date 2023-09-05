3d5ced37f63b4ca1c31e7a44af53ec04a6f0a986
Aleks37 <lao35180972643335589007>
2023-09-05 11:45:23 +0400

    Add Seminar9_Task2

---
+++ Seminar9_Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напиши прогу, к-ая по заданному № чертверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.WriteLine("Введите номер четверти. Число от 1 до 4!");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N == 1)
+{
+    Console.WriteLine("x>0, y>0");
+}
+else if (N == 2)
+{
+    Console.WriteLine("x<0, y>0");
+}
+else if (N == 3)
+{
+    Console.WriteLine("x<0, y<0");
+}
+else if (N == 4)
+{
+    Console.WriteLine("x>0, y<0");
+}
+else
+{
+    Console.WriteLine("Нужно было ввести Число от 1 до 4!");
+}
