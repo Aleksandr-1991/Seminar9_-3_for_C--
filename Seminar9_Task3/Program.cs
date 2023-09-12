88a98956b6a5386a0257d2df63ba2bcd873b8a6b
Aleks37 <lao35180972643335589007>
2023-09-12 20:25:14 +0400

    Add Changes for Seminar9_Task3

--- Seminar9_Task3/Program.cs
+++ Seminar9_Task3/Program.cs
@@ -1,8 +1,15 @@
 ﻿// Зад. №22 (в группах). Напиши прогу, к-ая принимает на вход число (N) 
 // и выдаёт на выход таблицу квадратов от 1 до N.
-Console.Write("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= N; i++)
+Console.Write("Введите натуральное число: ");
+Double N = Convert.ToDouble(Console.ReadLine());
+if (N < 1 || N%1 > 0)
 {
-    Console.WriteLine($"Для ", i, " квадрат = ", i*i);
+    Console.Write("Нужно было ввести Натуральное число!");
+}
+else 
+{
+    for (int i = 1; i <= N; i++)
+    {
+         Console.WriteLine($"Для {i} квадрат = {i*i};");
+    }
 }
