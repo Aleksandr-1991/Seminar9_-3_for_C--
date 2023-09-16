e8dcda072a0230d82a3e5573ce7f97db737a9244
Aleks37 <lao35180972643335589007>
2023-09-16 21:59:32 +0400

    From_16.09.23

--- Semonar9_HomeWork1/Program.cs
+++ Semonar9_HomeWork1/Program.cs
@@ -4,7 +4,7 @@
 // string [] array = new string[5];
 // for (int i = 0; i < array.GetLength(0); i++)
 // {
-//     Console.Write("Введите " + (i+1) + "-ю цифру пятизначного числа: ");
+//     Console.Write("Введите " + (i+1) + "-ю цифру пятичленного массива: ");
 //     array[i] = Console.ReadLine();
 // }
 // if (array[0] == array[4] && array[1] == array[3])
@@ -16,9 +16,14 @@
 //     Console.Write("Это Не палиндром.");
 // }
 
-Console.Write ("Введите пятизначное число: ");
 
-if (array[0] == array[4] && array[1] == array[3])
+Console.Write ("Введите пятизначное число (от 10'000 до 99'999 или от -10'000 до -99'999): ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N/100000 != 0 || N/1000 = 0)
+{
+    Console.Write("Не корректный ввод. Нужно было ввести пятизначное число (от 10'000 до 99'999 или от -10'000 до -99'999)!");
+}
+else if (N/10000 == N%10 && (N/1000)%10 == (N/10)%10)
 {
     Console.Write("Это палиндром.");
 }
