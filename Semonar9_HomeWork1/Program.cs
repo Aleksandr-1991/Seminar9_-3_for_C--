b324dea71960635fa16bd1f3ea7fdd9ff57717fd
Aleks37 <lao35180972643335589007>
2023-09-17 00:07:13 +0400

    Add_Changes_In_Seminar9_HomeWork1

--- Semonar9_HomeWork1/Program.cs
+++ Semonar9_HomeWork1/Program.cs
@@ -4,13 +4,35 @@
 
 // Применительно к массивам, которые могут содержать ещё и Буквы и Символы, код такой:
 
-// string [] array = new string[5];
-// for (int i = 0; i < array.GetLength(0); i++)
+string [] array = new string[5];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.Write("Введите " + (i+1) + "-ю цифру или символ пятичленного массива: ");
+    array[i] = Console.ReadLine();
+}
+Console.Write("Получился массив:  ");
+for (int j = 0; j < array.GetLength(0); j++)
+{
+    Console.Write(array[j]);
+}
+
+if (array[0] == array[4] && array[1] == array[3])
+{
+    Console.Write(" - Это палиндром.");
+}
+else 
+{
+    Console.Write(" - Это Не палиндром.");
+}
+   
+// Применительно к числам (только к пятизначым числам), код такой:
+// Console.WriteLine ("Введите пятизначное число (от 10'000 до 99'999 или от -10'000 до -99'999): ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N/100000 != 0 || N/10000 == 0)
 // {
-//     Console.Write("Введите " + (i+1) + "-ю цифру пятичленного массива: ");
-//     array[i] = Console.ReadLine();
+//     Console.Write("Не корректный ввод. Нужно было ввести пятизначное число!");
 // }
-// if (array[0] == array[4] && array[1] == array[3])
+// else if (N/10000 == N%10 && (N/1000)%10 == (N/10)%10 && N>0)
 // {
 //     Console.Write("Это палиндром.");
 // }
@@ -18,20 +40,3 @@
 // {
 //     Console.Write("Это Не палиндром.");
 // }
-
-
-// Применительно к числам (только к пятизначым числам), код такой:
-Console.WriteLine ("Введите пятизначное число (от 10'000 до 99'999 или от -10'000 до -99'999): ");
-int N = Convert.ToInt32(Console.ReadLine());
-if (N/100000 != 0 || N/10000 == 0)
-{
-    Console.Write("Не корректный ввод. Нужно было ввести пятизначное число!");
-}
-else if (N/10000 == N%10 && (N/1000)%10 == (N/10)%10 && N>0)
-{
-    Console.Write("Это палиндром.");
-}
-else 
-{
-    Console.Write("Это Не палиндром.");
-}
