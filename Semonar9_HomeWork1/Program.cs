6a1b52405b24c02dbff74cc1dac3b692fc40317d
Aleks37 <lao35180972643335589007>
2023-09-16 21:24:51 +0400

    FROM_16.09.23

---
+++ Semonar9_HomeWork1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19 (дом). Напишите прог-у, кот принимает на вход пятизначное число,
+// и проверяет яв-ся ли оно палиндромом.
+
+// string [] array = new string[5];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     Console.Write("Введите " + (i+1) + "-ю цифру пятизначного числа: ");
+//     array[i] = Console.ReadLine();
+// }
+// if (array[0] == array[4] && array[1] == array[3])
+// {
+//     Console.Write("Это палиндром.");
+// }
+// else 
+// {
+//     Console.Write("Это Не палиндром.");
+// }
+
+Console.Write ("Введите пятизначное число: ");
+
+if (array[0] == array[4] && array[1] == array[3])
+{
+    Console.Write("Это палиндром.");
+}
+else 
+{
+    Console.Write("Это Не палиндром.");
+}
